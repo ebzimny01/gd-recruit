43959e0fa3c9fbf17418376c6e848b8c551274f6
Ed Zimny <ebzimny@gmail.com>
2021-03-03 19:17:53 -0500

    Update browser.py
    trying to use this for browser actions that require authentication

--- mypackages/browser.py
+++ mypackages/browser.py
@@ -1,44 +1,76 @@
 from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
+import time
+import re
 
+def wis_browser(config, user, pwd, f):
 
-class WISPage:
-    def __init__(self, page):
-        self.page = page
-
-    def login(self, config):
-        username = config['WISCreds']['username']
-        password = config['WISCreds']['password']
-
-        self.page.set_viewport_size({"width": 1900, "height": 1200})
-        self.page.goto("http://idsrv.fanball.com/")
-        print(self.page.title())
-        
-        print("Authenticating to WIS...")
-        
-        # Click input[name="username"]
-        self.page.click("input[name=\"username\"]")
-        # Fill input[name="username"]
-        self.page.fill("input[name=\"username\"]", username)
-        
-        # Click input[name="password"]
-        self.page.click("input[name=\"password\"]")
-        # Fill input[name="password"]
-        self.page.fill("input[name=\"password\"]", password)
-        
-        # Click button:has-text("Sign in")
-        self.page.click("button:has-text(\"Sign in\")")
-
-        check_auth = self.page.text_content("Incorrect email or password", timeout=2000)
-        print(f"Value of check_auth = {check_auth}")
-
-        if check_auth is None:
-            return True
-        else:
-            return False
-
-def get_browser():
     with sync_playwright() as p:
         browser = p.firefox.launch(headless=False)
         page = browser.new_page()
-        wispage = WISPage(page)
-        return browser, wispage
+
+        page.set_viewport_size({"width": 1900, "height": 1200})
+        page.goto("https://www.whatifsports.com/locker/")
+        print(page.title())
+        # Click text=Login
+        page.click("text=Login")
+        # assert page.url == "https://idsrv.fanball.com/login?signin=2c7ce157635ba9eab815f3cb2bdb83ce"
+        # Go to https://idsrv.fanball.com/login?signin=2c7ce157635ba9eab815f3cb2bdb83ce&__cf_chl_jschl_tk__=748937e860f9dfc589364946f2e8af8c1eefbe7e-1614806310-0-AYX5rk621iTj_xPBtx5a9cubtsH4r7FR5uoSr4UmdL_BcUZKav9FAu1Wzybc7YI4a9N5Q7g_QAJzNzcOluUW-o99hqqOQQP1VkLwiP7W5DSaQYNqJBydxXiojR1tdAdzfnP6vQtkY42I0K7ngy-2AlArSUUiVG41fr4Y9rwHHatCLYPVhB3sTGZ17ZH8TCiXaNVC7pGYWav1fmxuY8lJ-iLb-ktGqxbLn8vV2EcrCNyZUzkeMk3ruMsoq0w-P_OTtzCltc-5vzq5SOKxnZyY84RvXRJai02utdOsiceCgMHsEWfVX0tNdHhq7tEW0lb4ABOTPwOkMuXX8WeczHUJHH35Lpxp0QzQ2QMuddSwXjS0vCfJTswNn8f8mAS_bP0GLcXHvOzXMVdD31TNyZvOUtKxmyYIoIHeKIDNZW3mvHtb
+        page.goto("https://idsrv.fanball.com/login?signin=2c7ce157635ba9eab815f3cb2bdb83ce&__cf_chl_jschl_tk__=748937e860f9dfc589364946f2e8af8c1eefbe7e-1614806310-0-AYX5rk621iTj_xPBtx5a9cubtsH4r7FR5uoSr4UmdL_BcUZKav9FAu1Wzybc7YI4a9N5Q7g_QAJzNzcOluUW-o99hqqOQQP1VkLwiP7W5DSaQYNqJBydxXiojR1tdAdzfnP6vQtkY42I0K7ngy-2AlArSUUiVG41fr4Y9rwHHatCLYPVhB3sTGZ17ZH8TCiXaNVC7pGYWav1fmxuY8lJ-iLb-ktGqxbLn8vV2EcrCNyZUzkeMk3ruMsoq0w-P_OTtzCltc-5vzq5SOKxnZyY84RvXRJai02utdOsiceCgMHsEWfVX0tNdHhq7tEW0lb4ABOTPwOkMuXX8WeczHUJHH35Lpxp0QzQ2QMuddSwXjS0vCfJTswNn8f8mAS_bP0GLcXHvOzXMVdD31TNyZvOUtKxmyYIoIHeKIDNZW3mvHtb")
+    
+        print("Authenticating to WIS...")
+        
+        # Click input[name="username"]
+        page.click("input[name=\"username\"]")
+        s = 0.50
+        print(f"Sleeping for {s} seconds...")
+        time.sleep(s)
+        # Fill input[name="username"]
+        page.fill("input[name=\"username\"]", user)
+        s = 0.65
+        print(f"Sleeping for {s} seconds...")
+        time.sleep(s)
+        # Click input[name="password"]
+        page.click("input[name=\"password\"]")
+        s = 0.75
+        print(f"Sleeping for {s} seconds...")
+        time.sleep(s)
+        # Fill input[name="password"]
+        page.fill("input[name=\"password\"]", pwd)
+        s = 0.85
+        print(f"Sleeping for {s} seconds...")
+        time.sleep(s)
+        # Click button:has-text("Sign in")
+        page.click("button:has-text(\"Sign in\")")
+
+        # assert page.url == "https://idsrv.fanball.com/localregistration/silentlogin"
+        # Go to https://idsrv.fanball.com/connect/authorize?acr_values=ConfirmEmailRedirectUrl%3Ahttps%3A%2F%2Fwww.whatifsports.com%2Faccount%2F&client_id=what-if-sports&nonce=637504031099333772.MDgxYWM3M2ItN2FkYS00ZjdmLWJmNjctNTlmYjZjMmZiNTFmMTUxNjkwM2UtMzcyOC00NDQyLTgzN2QtMTc0NTkzMjlhOTIy&redirect_uri=https%3A%2F%2Fwww.whatifsports.com%2Faccount%2F&response_mode=form_post&response_type=id_token%20token&scope=openid%20profile%20social%20email%20wallet-readonly%20whatifsports-readonly%20connect-notifications-publish&state=OpenIdConnect.AuthenticationProperties%3DS-1Qk-uKkfLVgpGPcL7yB-qOVViBx8fSkGL7Z45PIM7qxFQ8hnUs9nxV0jVW2PaOGGLS7m8ACLhiygen6m72BjLQENcZU-F4LJPVQ4QhhO52NOkRA8b2wJRq-DCL8pmkb15Pdc6cX-L0X1Ij2WzzqSYsZNCaE2y--tRR7O1qVoVqIk7d34s6wbOA_D_jxH6lmTD_dQ7tVNHDL4lE7mXSYfJZQOsGG9zMBhctiqj8MxNpLAWQlBTDuaV0NrE--rx17vBEJg&x-client-SKU=ID_NET461&x-client-ver=5.4.0.0
+        page.goto("https://idsrv.fanball.com/connect/authorize?acr_values=ConfirmEmailRedirectUrl%3Ahttps%3A%2F%2Fwww.whatifsports.com%2Faccount%2F&client_id=what-if-sports&nonce=637504031099333772.MDgxYWM3M2ItN2FkYS00ZjdmLWJmNjctNTlmYjZjMmZiNTFmMTUxNjkwM2UtMzcyOC00NDQyLTgzN2QtMTc0NTkzMjlhOTIy&redirect_uri=https%3A%2F%2Fwww.whatifsports.com%2Faccount%2F&response_mode=form_post&response_type=id_token%20token&scope=openid%20profile%20social%20email%20wallet-readonly%20whatifsports-readonly%20connect-notifications-publish&state=OpenIdConnect.AuthenticationProperties%3DS-1Qk-uKkfLVgpGPcL7yB-qOVViBx8fSkGL7Z45PIM7qxFQ8hnUs9nxV0jVW2PaOGGLS7m8ACLhiygen6m72BjLQENcZU-F4LJPVQ4QhhO52NOkRA8b2wJRq-DCL8pmkb15Pdc6cX-L0X1Ij2WzzqSYsZNCaE2y--tRR7O1qVoVqIk7d34s6wbOA_D_jxH6lmTD_dQ7tVNHDL4lE7mXSYfJZQOsGG9zMBhctiqj8MxNpLAWQlBTDuaV0NrE--rx17vBEJg&x-client-SKU=ID_NET461&x-client-ver=5.4.0.0")
+        # Go to https://www.whatifsports.com/locker/lockerroom.asp
+        print("Going to Lockerroom page...")
+        page.goto("https://www.whatifsports.com/locker/lockerroom.asp")
+        
+        if "updateteams" in f:
+            # page.goto("https://www.whatifsports.com/locker/lockerroom.asp")
+            s = 10
+            print(f"Sleeping for {s} seconds...")
+            time.sleep(s)
+            contents = page.content()
+            lockerroom_soup = BeautifulSoup(contents, "lxml")
+            select_teamName_divs = lockerroom_soup.find_all(class_="teamName")
+            baseURL = "../gd/TeamRedirect.aspx?tid="
+            for team in select_teamName_divs:
+                a_tag = team.find("a")
+                link = a_tag.attrs['href']
+                if baseURL in link:
+                    school = a_tag.text
+                    school_id_re = re.search(r'(\d{5})', link)
+                    school_id = school_id_re.group(1)
+                    if config.has_section('Schools'):
+                        config.set('Schools', school_id, school)
+                    else:
+                        config.add_section('Schools')
+                        config.set('Schools', school_id, school)
+
+            with open('config.ini', 'w') as file:
+                config.write(file)
