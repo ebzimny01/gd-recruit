15deaad8abfc3aa8daf011141bee407a11908d2c
Ed Zimny <ebzimny@gmail.com>
2021-03-04 12:29:05 -0500

    various changes
    1. Added INitialize Reruit to ui and some functional code
    2. Started implementing some of the workflow and logic to create and load database and update it.
    3. The New Season and Load Season fields auto-populate dropdowns based on certain content.
    
    In general the initializing DB part is not working so you can't get a DB loaded into the table view yet.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import asyncio
+from mypackages.initialize_recruits_widget import Ui_WidgetInitializeRecruits
+import os
 import sys
+import asyncio
 from playwright.async_api import async_playwright
 from PySide2.QtCore import *
 from PySide2.QtGui import *
@@ -8,14 +10,111 @@ from PySide2.QtSql import *
 from mypackages.mainwindow_ui import Ui_MainWindow
 from mypackages.wis_cred_dialog import Ui_WISCredentialDialog
 from mypackages.new_season_dialog import Ui_DialogNewSeason
+from mypackages.load_season_dialog import Ui_DialogLoadSeason
 from mypackages.world_lookup import wid_world_list
 from mypackages.browser import *
 import configparser
+from progress.bar import Bar
 
 
 # https://stackoverflow.com/questions/61316258/how-to-overwrite-qdialog-accept
 
 
+def initialize_recruit_data(config, user, pwd):
+    requests_session = requests.Session()
+    wis_browser(config, user, pwd, "scrape_recruit_IDs", db)
+    if not db.open():
+        QMessageBox.critical(
+            None,
+            "GD Recruiting App - Error!",
+            "Database Error: %s" % db.lastError().databaseText()
+            )
+        sys.exit(1)
+    query = QSqlQuery()
+    query.exec_("SELECT id FROM recruits")
+    rids = []
+    while query.next():
+        rids.append(query.value(id))
+    query.finish()
+    queryUpdate = QSqlQuery()
+    with Bar('Initializing Recruit Static Data without Playwright', max=len(rids)) as bar:
+        for rid in rids:
+            recruitpage = requests_session.get(f"https://www.whatifsports.com/gd/RecruitProfile/Ratings.aspx?rid={rid}")
+            recruitpage_soup = BeautifulSoup(recruitpage.content, "lxml")
+            # name_section = recruitpage_soup.find(id="ctl00_ctl00_ctl00_Main_Main_name")
+            # name = name_section.text
+            # pos_section = recruitpage_soup.find(id="ctl00_ctl00_ctl00_Main_Main_position")
+            # pos = pos_section.text
+            recruit_ratings_section = recruitpage_soup.find(class_="ratingsDisplayCtl")
+            recruit_ratings_values = recruit_ratings_section.find_all(class_="value")
+            ath = int(recruit_ratings_values[0].text)
+            spd = int(recruit_ratings_values[1].text)
+            dur = int(recruit_ratings_values[2].text)
+            we = int(recruit_ratings_values[3].text)
+            sta = int(recruit_ratings_values[4].text)
+            strength = int(recruit_ratings_values[5].text)
+            blk = int(recruit_ratings_values[6].text)
+            tkl = int(recruit_ratings_values[7].text)
+            han = int(recruit_ratings_values[8].text)
+            gi = int(recruit_ratings_values[9].text)
+            elu = int(recruit_ratings_values[10].text)
+            tec = int(recruit_ratings_values[11].text)
+            tot = ath + spd + dur + we + sta + strength + blk + tkl + han + gi + elu + tec
+            gpa_section = recruitpage_soup.find(id="ctl00_ctl00_ctl00_Main_Main_gpa")
+            gpa = float(gpa_section.text)
+            # hometown_section = recruitpage_soup.find(id="ctl00_ctl00_ctl00_Main_Main_homeTown")
+            # hometown = hometown_section.text
+            queryUpdate.exec_(
+                        f"""
+                        UPDATE recruits SET ath = {ath} ,
+                                            spd = {spd} ,
+                                            dur = {dur} ,
+                                            we = {we} ,
+                                            sta = {sta} ,
+                                            str = {strength} ,
+                                            blk = {blk} ,
+                                            tkl = {tkl} ,
+                                            han = {han} ,
+                                            gi = {gi} ,
+                                            elu = {elu} ,
+                                            tec = {tec} ,
+                                            tot = {tot} ,
+                                            gpa = {gpa}
+                            WHERE id = {rid}
+                        """
+            )
+            bar.next()
+    queryUpdate.finish()
+    db.close()
+
+
+class InitializeRecruits(QDialog, Ui_WidgetInitializeRecruits):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        user = config['WISCreds']['username']
+        pwd = config['WISCreds']['password']
+        initialize_recruit_data(config, user, pwd)
+    
+    def accept(self):
+        # Need to add functionality for loading season
+        super().accept()
+
+class LoadSeason(QDialog, Ui_DialogLoadSeason):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        db_files = [x for x in os.listdir() if x.endswith(".db")]
+        if len(db_files) > 0:
+            self.comboBoxSelectSeason.addItems(db_files)
+
+    def accept(self):
+        # Need to add functionality for loading season
+        season_filename = self.comboBoxSelectSeason.currentText()
+        db.setDatabaseName(season_filename)
+        super().accept()
 
 class NewSeason(QDialog, Ui_DialogNewSeason):
     def __init__(self, parent=None):
@@ -24,16 +123,61 @@ class NewSeason(QDialog, Ui_DialogNewSeason):
         config = configparser.ConfigParser()
         config.read('config.ini')
         if config.has_section('Schools') and len(config['Schools']) > 0:
-            i = 0
-            for id in config['Schools']:
-                self.comboBoxTeamID.setItemText(i, QCoreApplication.translate("DialogNewSeason", f"{id}", None))
-                i += 1
+            self.comboBoxTeamID.addItems(config['Schools'])
+            #i = 0
+            #for id in config['Schools']:
+                #self.comboBoxTeamID.addItem(f"{id}")
+                # self.comboBoxTeamID.setItemText(i, QCoreApplication.translate("DialogNewSeason", f"{id}", None))
+                #i += 1
 
     def accept(self):
-        teamID = self.lineEditTeamID.text()
+        teamID = self.comboBoxTeamID.currentText()
         seasonnum = self.lineEditSeasonNumber.text()
         wid_world = wid_world_list()
         world = wid_world[teamID]
+        # Need to add functionality for New Season
+        season_filename = f"{world} {seasonnum} - {teamID}.db"
+        db.setDatabaseName(season_filename)
+        if not db.open():
+            QMessageBox.critical(
+                self,
+                "GD Recruiting App - Error!",
+                "Database Error: %s" % db.lastError().databaseText()
+                )
+            sys.exit(1)
+        createRecruitTableQuery = QSqlQuery()
+        createRecruitTableQuery.exec_(
+            """
+            CREATE TABLE IF NOT EXISTS recruits (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                pos TEXT,
+                height TEXT,
+                weight INTEGER,
+                rating INTEGER,
+                rank INTEGER,
+                hometown TEXT,
+                miles INTEGER,
+                considering TEXT,
+                ath INTEGER,
+                spd INTEGER,
+                dur INTEGER,
+                we INTEGER,
+                sta INTEGER,
+                str INTEGER,
+                blk INTEGER,
+                tkl INTEGER,
+                han INTEGER,
+                gi INTEGER,
+                elu INTEGER,
+                tec INTEGER,
+                tot INTEGER,
+                gpa REAL,
+                pot TEXT
+            )
+            """
+        )
+        db.close()
         super().accept()
 
 
@@ -68,6 +212,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         v_header.setSectionResizeMode(QHeaderView.ResizeToContents)
         self.actionWIS_Credentials.triggered.connect(self.open_WIS_cred)
         self.actionNew_Season.triggered.connect(self.open_New_Season)
+        self.actionLoad_Season.triggered.connect(self.open_Load_Season)
+        self.actionInitialize_Recruits.triggered.connect(self.open_Initialize_Recruits)
         config = configparser.ConfigParser()
         config.read('config.ini')
         if self.check_stored_creds(config):
@@ -77,27 +223,42 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             user = config['WISCreds']['username']
             pwd = config['WISCreds']['password']
             f = "updateteams"
-            # wis_browser(config, user, pwd, f)
+            # wis_browser(config, user, pwd, f, db)
         else:
             False
             
 
     def open_WIS_cred(self):
         dialog = WISCred()
-        dialog.ui = Ui_WISCredentialDialog()
-        #dialog.ui.setupUi(dialog)
-        
+        dialog.ui = Ui_WISCredentialDialog()        
         dialog.exec_()
         dialog.show()
-        self.check_stored_creds()
+        config = configparser.ConfigParser()
+        config.read('config.ini')
+        self.check_stored_creds(config)
 
 
     def open_New_Season(self):
         dialog = NewSeason()
         dialog.ui = Ui_DialogNewSeason()
-        # dialog.ui.setupUi(dialog)
         dialog.exec_()
         dialog.show()
+
+
+    def open_Load_Season(self):
+        dialog = LoadSeason()
+        dialog.ui = Ui_DialogLoadSeason()
+        dialog.exec_()
+        dialog.show()
+        self.setWindowTitle(f"GD Recruit Helper - {db.databaseName()}")
+
+
+    def open_Initialize_Recruits(self):
+        dialog = InitializeRecruits()
+        dialog.ui = Ui_WidgetInitializeRecruits()
+        dialog.exec_()
+        dialog.show()
+
 
     def check_stored_creds(self, config):
         if config['WISCreds']['username'] == '' or config['WISCreds']['password'] == '':
@@ -165,7 +326,7 @@ def initializeModel(model):
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     db = QSqlDatabase.addDatabase('QSQLITE')
-    db.setDatabaseName("recruit.db")
+    # db.setDatabaseName("recruit.db")
     model = QSqlTableModel()
     initializeModel(model)
     mw = MainWindow()
